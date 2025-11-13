02a4cb09441632ad1aa65dd72ef187a0d9106551
Felix Canditt <felix.canditt@gmail.com>
2025-11-13 11:48:05 +0100

    installed prettier

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,18 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('repo-list-angular');
+
+  constructor() {
+    this.log();
+  }
+
+  log() {
+    console.log(1);
+  }
+
+  count = 1;
 }
